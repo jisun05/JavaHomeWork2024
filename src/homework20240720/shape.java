1e9a9e35d754c4ed873d192a944e9d6fc65f9d72
Adam-Steven15 <adamsteven15+tutoring@gmail.com>
2024-07-21 09:39:09 +0100

    Added a file to make your life difficult

---
+++ src/homework20240720/shape.java
@@ -0,0 +1,14 @@
+package homework20240720;
+
+public abstract class Shape {
+
+    private String name;
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+}
