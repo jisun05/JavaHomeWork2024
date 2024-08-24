f9a0d5748c00a241a57f94452c50c93fb44cb976
jisun05 <jisunnala@gmail.com>
2024-08-25 06:35:36 +0900

    -homework20240825

---
+++ src/homework20240825/JList.java
@@ -0,0 +1,35 @@
+package homework20240825;
+
+public class JList {
+
+
+    Node head;
+
+    public JList() {
+        this.head = null;
+    }
+
+
+    public void add(int data) {
+
+    }
+
+
+    public void remove(int data) {
+
+    }
+
+    public boolean search(int data) {
+        return false;
+    }
+
+
+    public void printList() {
+
+
+    }
+
+}
+
+
+
---
+++ src/homework20240825/Main.java
@@ -0,0 +1,10 @@
+package homework20240825;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
