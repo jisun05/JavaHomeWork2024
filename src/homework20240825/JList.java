c7dd390562d68bc2c8042b3c8aaf1fb189a25f44
jisun05 <jisunnala@gmail.com>
2024-08-31 09:15:40 +0900

    -homework20240825

--- src/homework20240825/JList.java
+++ src/homework20240825/JList.java
@@ -2,15 +2,27 @@ package homework20240825;
 
 public class JList {
 
-
+    int size = 0;
     Node head;
 
     public JList() {
         this.head = null;
+        this.size = 0;
     }
 
 
     public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
 
     }
 
--- src/homework20240825/Main.java
+++ src/homework20240825/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        JList jList = new JList();
 
+        jList.add(3);
+        jList.add(9);
+        jList.add(1);
     }
 }
